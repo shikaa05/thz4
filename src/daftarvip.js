2f8b599e1a88f64ca94f4fb47b944d7379550b55
shikaa05 <theumtf505@gmail.com>
2021-05-20 12:08:42 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/558191153393 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO SHIKAA 🐊🚩 :*
+_ https://chat.whatsapp.com/KvCtL8ZMFQq3K65T5kEaHI `
+}
+exports.daftarvip = daftarvip
---
+++ src/lista.js
@@ -0,0 +1,24 @@
+const lista = () => { 
+	return `
+	#RenegadosDeKonohaRDK
+
+Líderes:
+
+➥ Sr Shika: https://youtube.com/channel/UCiAOyGlqZffoBZQkp44g8BA
+_____________________________________
+
+VIce Lider:
+
+➥ Sr Lnz:
+https://youtube.com/channel/UCPz9e_tOgb8TS5WZtm9R5Iw
+
+_____________________________________
+
+Membros:
+
+➥ Tioh Rn8: https://youtube.com/channel/UCxzanIBjpdsk8Boe_8G4jgQ
+
+➥ Tioh Mxz: Tem canal nn :/
+`
+}
+exports.lista = lista
